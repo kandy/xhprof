d9fe52f14b49a2c4a86a36f74be4e81571cade70
Andrii Kasian <akasian@ebay.com>
2013-12-26 16:00:00 +0200

    Add base grid support

--- src/init.php
+++ src/init.php
@@ -3,13 +3,15 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['params'] = require (__DIR__.'/../config/config.dist.php');
+$app['debug'] = true;
+$configFile = __DIR__ . '/../config/config.dist.php';
+$app['params'] = require (file_exists($configFile) ? $configFile : __DIR__ . '/../config/config.dist.php');
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app['base_path'] = 'http://localhost/workspace/xhprof.me/web/';
+$app['base_path'] =  dirname($_SERVER['SCRIPT_NAME']) . '/';
 $app['twig'] = $app->share(
     $app->extend(
         'twig',
@@ -17,7 +19,6 @@ $app['twig'] = $app->share(
             $twig->addFunction(
                 new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
                     // implement whatever logic you need to determine the asset path
-
                     return sprintf($app['base_path'] .'%s', ltrim($asset, '/'));
                 })
             );
@@ -27,13 +28,9 @@ $app['twig'] = $app->share(
     )
 );
 $app['db'] = $app->share(function($app){
-
     $db = new PDO($app['params']['dsn']);
-    // Set errormode to exceptions
-    $db->setAttribute(
-        PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION
-    );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 });
+
 return $app;
